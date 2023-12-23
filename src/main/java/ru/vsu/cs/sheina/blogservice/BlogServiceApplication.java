b58b93b3090dc7c00c545e9692af4428c33d27a1
undine_20 <lena_sheina@bk.ru>
2023-12-23 12:42:38 +0300

    initial commit

---
+++ src/main/java/ru/vsu/cs/sheina/blogservice/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.sheina.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServiceApplication.class, args);
+    }
+
+}
